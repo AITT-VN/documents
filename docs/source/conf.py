8a284a615ad97ad39cb857656be245ce5c473757
dangthanhtri <48786737+dangthanhtri@users.noreply.github.com>
2023-11-06 22:33:43 +0700

    Re-Migrate to V2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,60 +1,36 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
-
+# -- Project information
 project = 'OhStem Education Wiki'
 copyright = '2022, OHSTEM'
 author = 'OHSTEM EDUCATION'
 
-# The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '2.0'
+version = '2.0.0'
 
+# -- General configuration
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-#html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme' # pip3 install sphinx_rtd_theme
 
 html_logo = 'images/logo-ohstem-white.png'
 
-# The name of an image file (within the static path) to use as favicon of the
-# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
-# pixels large.
+
 html_favicon = 'images/favicon.ico'
 
 html_theme_options = {
@@ -97,3 +73,6 @@ html_static_path = ['_static']
 html_css_files = [
     'css/custom.css',
 ]
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
